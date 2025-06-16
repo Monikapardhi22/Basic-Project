664de41379d9f7cde596e2f0e425d5fcd13ba496
Monikapardhi22 <monikapardhi277@gmail.com>
2025-06-16 11:12:35 -0400

    add the file

---
+++ basic.js
@@ -0,0 +1,57 @@
+// after submission
+
+let submit  = document.querySelector(".sub");
+
+submit.onclick =()=>{
+   let x=document.querySelector("#sub1");
+   x.innerText = "Thank you for sign in";
+}
+
+
+// explore now
+let btn=document.querySelector(".bt");
+btn.onclick=()=>{
+    let bt=document.querySelector(".bt");
+    bt.style .display="none";
+
+
+   let line=document.querySelector(".demo");
+   line.innerText ="Wellcome  Friend";
+   line.style .color ="white";
+   line.style .fontSize="19px";
+   line.style .padding ="10px";
+
+
+}
+
+
+// for click here
+
+let click=document.querySelector("#click");
+
+click.onclick=()=>{
+   
+   click.style.display="none";
+   
+
+}
+
+
+// bear in explore page
+
+let note=document.querySelector(".img6");
+note.onmouseenter=()=>{
+   let no=document.querySelector(".note");
+  
+   no.style .width="100px";
+  
+
+}
+
+
+
+
+
+
+
+
